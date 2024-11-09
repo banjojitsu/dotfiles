8f5bb0cb4c6b614d0ca60f5a888e7aeaf2cc037b
jason <>
2024-11-09 10:48:02 -0500

    update configs

---
+++ .config/nvim/lua/plugins/mini-files.lua
@@ -0,0 +1,22 @@
+return {
+	"echasnovski/mini.files",
+	version = false,
+	lazy = false,
+	opts = {
+		mappings = {
+			go_in = "<right>",
+			go_out = "<left>",
+		},
+		options = {
+			use_as_default_explorer = true,
+		},
+	},
+	keys = {
+		{
+			"`",
+			"<cmd>lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<cr>",
+			mode = "",
+			desc = "View directory",
+		},
+	},
+}
