a0124311a8a7f57de6d05501d5d7121ed7f36cef
jason <>
2024-11-09 11:57:12 -0500

    update configs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,9 @@
-local opt = vim.opt
+local g = vim.g
+-- Disable netrw, using mini-files
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
 
+local opt = vim.opt
 opt.cursorline = true -- Show which line cursor is on
 opt.expandtab = true -- Use spaces instead of tabs
 opt.ignorecase = true -- Ignore case when searching
