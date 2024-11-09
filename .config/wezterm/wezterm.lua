d55d7487133621bc7925384580ad808b43380f42
jason <>
2024-11-08 21:54:01 -0500

    update configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "AdventureTime"
+config.color_scheme = "Gruvbox Dark (Gogh)"
 config.enable_scroll_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
