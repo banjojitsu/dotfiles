765558f360b95783eb24856dbc596b6ad48c530d
jason <>
2024-11-09 15:00:35 -0500

    update configs

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,12 +3,12 @@ local map = vim.keymap.set
 -- General settings
 map("n", "<esc>", "<cmd>nohlsearch<cr>")
 map("n", "<leader><leader>", ":", { desc = "::" })
+map("n", "<leader>c", "<cmd>bd!<cr>", { desc = "Close current buffer" })
 map("n", "<leader>q", "<cmd>q!<cr>", { desc = "Quit neovim" })
 map("n", "<leader>r", ":%s/<c-r><c-w>", { desc = "Search & replace word" })
 map("n", "<leader>w", "<cmd>w<cr>", { desc = "Write file" })
 map("x", "<leader>r", 'y:%s/<c-r>"', { desc = "Replace selection" })
 map("x", "<leader>s", ":sort<cr>", { desc = "Sort selection" })
-map("n", "<leader>c", "<cmd>bd!<cr>", { desc = "Close current buffer" })
 
 -- Conform
 local conform = require("conform")
@@ -18,10 +18,10 @@ end, { desc = "Format buffer" })
 
 -- Telescope
 local builtin = require("telescope.builtin")
+map("n", "<leader>td", builtin.diagnostics, { desc = "Search diagnostics" })
 map("n", "<leader>tf", builtin.find_files, { desc = "Find files" })
 map("n", "<leader>tg", builtin.live_grep, { desc = "Live grep" })
 map("n", "<leader>ts", builtin.current_buffer_fuzzy_find, { desc = "Search current file" })
-map("n", "<leader>td", builtin.diagnostics, { desc = "Search diagnostics" })
 
 -- Telescope LSP
 map("n", "<leader>ls", builtin.lsp_document_symbols, { desc = "Document symbols" })
