c5900060b56441312c89c820af73e18934ea419c
jason <>
2024-11-08 20:57:51 -0500

    update wezterm configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,9 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "AdventureTime"
-config.window_decorations = "RESIZE"
 config.enable_scroll_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "RESIZE"
 
 return config
