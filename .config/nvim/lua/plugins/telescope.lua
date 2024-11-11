a15a692581d0b08e8fff8d777dd067c051571fc2
jason <>
2024-11-11 11:40:19 -0500

    update telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,6 +6,6 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	opts = {
-		defaults = require("telescope.themes").get_ivy({ layout_config = { height = 0.5, preview_cutoff = 10 } }),
+		defaults = require("telescope.themes").get_ivy({ layout_config = { height = 0.5, preview_cutoff = 0 } }),
 	},
 }
