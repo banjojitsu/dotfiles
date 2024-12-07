8ad22668fcd67ec557c7375bdfe921eace3349e6
jason <105950172+banjojitsu@users.noreply.github.com>
2024-12-06 23:29:34 -0500

    update configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,9 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
 		--		defaults = require("telescope.themes").get_ivy({ layout_config = { height = 0.5 } }),
-		defaults = require("telescope.themes").get_dropdown({ layout_config = { preview_cutoff = 100 } }),
+		defaults = require("telescope.themes").get_dropdown({
+			layout_config = { preview_cutoff = 100 },
+		}),
 	},
 	keys = {
 		{ "<leader>t", "<cmd>Telescope<cr>", desc = "telescope" },
