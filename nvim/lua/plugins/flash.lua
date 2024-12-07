ac09244323890b7b54d89f82e2d2c79769b4d4e3
jason <105950172+banjojitsu@users.noreply.github.com>
2024-12-06 20:31:58 -0500

    add more plugins

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"<leader>s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "flash",
+		},
+	},
+}
