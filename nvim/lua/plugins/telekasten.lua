00582cc8bb6fd8e6ecf4be46ba9d759a33992800
jason <105950172+banjojitsu@users.noreply.github.com>
2024-12-06 20:42:26 -0500

    sorted telekasten opts

--- nvim/lua/plugins/telekasten.lua
+++ nvim/lua/plugins/telekasten.lua
@@ -2,20 +2,18 @@ return {
 	"renerocksai/telekasten.nvim",
 	dependencies = { "nvim-telescope/telescope.nvim" },
 	opts = {
-		home = vim.fn.expand("~/Library/Mobile Documents/com~apple~CloudDocs/zk"),
-
-		new_note_filename = "uuid-title",
-		uuid_type = "rand",
-		uuid_sep = "|",
-
-		command_palette_theme = "dropdown",
 		auto_set_filetype = false,
-		filename_space_subst = "-",
+		command_palette_theme = "dropdown",
 		filename_small_case = true,
-		sort = "modified",
+		filename_space_subst = "-",
 		follow_creates_nonexisting = false,
-
+		home = vim.fn.expand("~/Library/Mobile Documents/com~apple~CloudDocs/zk"),
+		new_note_filename = "uuid-title",
+		show_tags_theme = "dropdown",
+		sort = "modified",
 		template_new_note = vim.fn.expand("~/Library/Mobile Documents/com~apple~CloudDocs/zk/templates/new.md"),
+		uuid_sep = "|",
+		uuid_type = "rand",
 	},
 	keys = {
 		{ "<leader>z", "<cmd>Telekasten panel<cr>", desc = "telekasten" },
